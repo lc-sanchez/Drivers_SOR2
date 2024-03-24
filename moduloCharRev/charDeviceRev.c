466ba9e400d7d896f09433687ab3a5089a82f7dd
lc-sanchez <csanchez_98@outlook.com>
2024-03-24 19:04:39 -0300

    agregando carpeta moduloCharRev para mejor orden

--- moduloCharRev/charDeviceRev.c
+++ moduloCharRev/charDeviceRev.c
@@ -28,11 +28,11 @@ struct file_operations fops = {
 of the device as it will appear in /proc/devices and struct fops is the pointer to the file_
 operations table for your driver.*/
 
-#define DEVICE_NAME "charDevice" //Dev name as it appears in /proc/devices
+#define DEVICE_NAME "charDeviceRev" //Dev name as it appears in /proc/devices
 #define SUCCESS 0
 #define PROCFS_MAX_SIZE 80 //max lenght of the msg from the device
 
-/*Global variables are declared static, so are global within the file*/
+//Global variables are declared static, so are global within the file
 static int major; //major number assigned to our device driver
 static int Device_open = 0; //used to prevent multiple access to device
 
@@ -55,7 +55,7 @@ int init_module(void)
     return SUCCESS;
 }
 
-/*When the module is unloaded*/
+/*when the module is unloaded*/
 void cleanup_module(void)
 {
     //se saca del registro al device
@@ -63,9 +63,8 @@ void cleanup_module(void)
     pr_info("Se ha quitado el modulo %s. \n",DEVICE_NAME);
 }
 
-//Metodos
-
-/*Cuando a process tries to open the device file like "cat /dev/mycharfile" */
+//metodos
+/*Cuando a process tries to open the device file like "cat /dev/mycharfile"*/
 static int device_open(struct inode *inode, struct file *file)
 {
     if(Device_open){
@@ -83,6 +82,28 @@ static int device_release(struct inode *inode, struct file *file)
     return SUCCESS;
 }
 
+/*Method to reverse a msg*/
+void reverso(char *buffer){
+    int i=0;
+    int j=procfs_buffer_size-2; 
+    char letra;
+
+    if(buffer == NULL){
+        return; //No hay msg que procesar
+    }
+
+    //Ahora vamos intercambiando las letras
+    while(i<j){
+        letra = buffer[i];
+        buffer[i]=buffer[j];
+        buffer[j]=letra;
+        i++;
+        j--;
+    }
+
+}
+
+
 /*When a process, which already opened the dev file, attempts to read from it*/
 static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
 {    
@@ -91,25 +112,23 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_
         return SUCCESS; //termino lectura
     }
 
-    /*se copia el contenido del kernel al user space */
+    
+    /*se copia el contenido del kernel al user space*/
     if (copy_to_user(buffer,procfs_buffer,procfs_buffer_size)){
         return -EFAULT; //si hay error al copiar los datos
     }
     
-    /*Actualizamos la posicion del offset*/
+    //Damos vuelta el mensaje
+    reverso(buffer);
+    
+    //Actualizamos la posicion del offset
     *offset += procfs_buffer_size;
 
     pr_info("Se lee lo siguiente: %s \n",buffer);
     return procfs_buffer_size;
 }
 
-/*Called when a process writes to dev file: echo "hi" > dev/hello 
-puntero a estructura file sobre el archivo q se esta realizando la escritura
-buffer contiene los datos q se van a escribir en el device, se encuentra en el user space
-y se deben copiar al espacio del kernel para procesarlo
-len long de los datos q se van a escribir, indica cuando bytes del buffer deben ser escritos en el device
-offset puntero de desplazamiento actual del archivo, indica donde se deben escribir los datos dentro
-del device */
+/*Method to write*/
 static ssize_t device_write(struct file *filp, const char __user *buff, size_t len, loff_t *off)
 {   
     procfs_buffer_size = len;
@@ -117,16 +136,17 @@ static ssize_t device_write(struct file *filp, const char __user *buff, size_t l
         procfs_buffer_size = PROCFS_MAX_SIZE;
     }
 
-    /*Limpiamos el buffer del kernel para la nueva escritura*/
+    //limpiamos el buffer del kernel para la nueva escritura
     memset(procfs_buffer,0,sizeof procfs_buffer);
     
-    /*se copia datos del user space al kernel space*/
+    //se copia datos del user space al kernel space
+    //to, from, tamaño
     if (copy_from_user(procfs_buffer,buff,procfs_buffer_size)){
         return -EFAULT;
     }
-    /*Agregamos elemento nulo*/
+    //Agregamos elemento nulo
     procfs_buffer[procfs_buffer_size]='\0';
-    /*Actualizamos desplazamiento*/
+    //Actualizamos desplazamiento
     *off+=procfs_buffer_size;
     
     pr_info("Se ha escrito correctamente: %s \n", procfs_buffer);
